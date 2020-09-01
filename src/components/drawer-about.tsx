db758dea09af5e81fd036430b69c97dc2e529cf4
zmen <sandoverflow@gmail.com>
2020-09-01 11:21:20 +0800

    feat: read github auth from config

--- src/components/drawer-about.tsx
+++ src/components/drawer-about.tsx
@@ -2,10 +2,22 @@ import React, { useContext } from 'react';
 import { Drawer } from 'antd';
 import { useTranslation } from 'gatsby-plugin-react-i18next';
 import GithubUserInfo from './github-user-info';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import VisibilityContext from '../context/VisibilityContext';
 
 const DrawerAbout = () => {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            githubAuthToken
+          }
+        }
+      }
+    `
+  );
   const { t } = useTranslation();
   const { state: { isAboutDialogVisible }, dispatch } = useContext(VisibilityContext);
 
@@ -18,7 +30,7 @@ const DrawerAbout = () => {
       visible={isAboutDialogVisible}
       style={{ position: 'absolute' }}
     >
-      <GithubUserInfo auth={null} />
+      <GithubUserInfo auth={site.siteMetadata.githubAuthToken} />
     </Drawer>
   );
 };
