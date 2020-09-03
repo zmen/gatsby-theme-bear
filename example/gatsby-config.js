eb5995120609bf88921fd872726eda1d2e7dcc56
zmen <sandoverflow@gmail.com>
2020-09-03 17:23:27 +0800

    feat: add prefix path to build command

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     author: 'Zmen',
     githubAuthToken: '3af6a94a8059f9771d6ec6429d5a14639b1b0791',
   },
+  pathPrefix: `/gatsby-theme-bear`,
   plugins: [{
     resolve: 'gatsby-theme-bear',
     options: {
